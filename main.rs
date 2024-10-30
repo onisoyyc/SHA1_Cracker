4b0df3a36b4b0fba8676ee67f8de0c21a7ccbc78
Daniel <64721730+onisoyyc@users.noreply.github.com>
2024-10-30 16:02:57 -0600

    Update main.rs with complete code

--- main.rs
+++ main.rs
@@ -14,7 +14,8 @@ fn main() -> Result<(), Box<dyn Error >>{
 
     if args.len() != 3 {
         println!("Usage:");
-        println!("sha1_cracker: <wordlist.txt> <sha1_hash>"); // build to allow user to input path to a file of hashes
+        println!("sha1_cracker: </path/to/wordlist.txt> <sha1_hash>"); // build to allow user to input path to a file of hashes
+        println!("Example: cargo run -- rockyou.txt 7c6a61c68ef8b9b6b061b28c348bc1ed7921cb53"); //prints Hello, World!
         return;
     }
 
@@ -28,11 +29,16 @@ fn main() -> Result<(), Box<dyn Error >>{
     let reader = BufReader::new(&wordlist_file);
 
     for line in reader.lines() {
-        let line = 
-        println!("{}", line);
+        let line = line?;
+        let common_password = line.trim();
+        if hash_to_crack == &hex::encode(sha1::Sha1::digest(common_password.as_bytes())) {
+            println!("Password found: {}", &common_password);
+            return OK(());
+        }
+        // println!("{}", line);
     }
 
-
+    println!("password not found: {}", &common_password);
 
     Ok(())
 }
